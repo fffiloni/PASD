5781a82e8e2bee5ce5638f01ed59f18dbfa501ff
Sylvain Filoni <sylvain.fillion15@gmail.com>
2024-01-12 19:31:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
-diffusers=0.21.4
-accelerate
-transformers
-xformers
-basicsr
-ultralytics
-salesforce-lavis
-webdataset
+from setuptools import setup, find_packages
+
+setup(
+    name='PASD',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies from the PASD repository requirements.txt file
+        'diffusers=0.21.4',
+        'accelerate',
+        'transformers',
+        'xformers',
+        'basicsr',
+        'ultralytics',
+        'salesforce-lavis',
+        'webdataset',
+        # Add more dependencies as needed
+    ],
+)
+
+
